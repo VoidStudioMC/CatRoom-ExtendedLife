eaea4e0d45de2ee4d0d0478ac38df100d47aaf91
JustCursed <mytojimbyylabu@gmail.com>
2025-10-03 13:15:56 +0300

    rollback patch for `ClassInheritanceProvider`

--- src/main/java/catserver/server/remapper/ClassInheritanceProvider.java
+++ src/main/java/catserver/server/remapper/ClassInheritanceProvider.java
@@ -1,12 +1,7 @@
 package catserver.server.remapper;
 
 import net.md_5.specialsource.provider.InheritanceProvider;
-import net.minecraftforge.fml.common.asm.transformers.deobf.FMLDeobfuscatingRemapper;
-import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.ClassVisitor;
-import org.objectweb.asm.Opcodes;
 
-import java.io.InputStream;
 import java.util.Collection;
 import java.util.HashSet;
 
@@ -16,61 +11,25 @@ public class ClassInheritanceProvider implements InheritanceProvider {
     @Override
     public Collection<String> getParents(String className) {
         className = ReflectionTransformer.remapper.map(className);
-        FMLDeobfuscatingRemapper mapper = FMLDeobfuscatingRemapper.INSTANCE;
-        String obfName = mapper.unmap(className);
-        try (InputStream is = this.getClass().getClassLoader().getResourceAsStream((obfName + ".class"))) {
-            if (is == null) {
-                return null;
-            }
 
-            ClassReader reader = new ClassReader(is);
-            ParentVisitor visitor = new ParentVisitor();
-
-            reader.accept(visitor, ClassReader.SKIP_CODE | ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES);
-
+        try {
             Collection<String> parents = new HashSet<>();
-
-            if (visitor.getSuperName() != null && !visitor.getSuperName().equals("java/lang/Object")) {
-                parents.add(reverseMap(mapper.map(visitor.getSuperName())));
+            Class<?> reference = Class.forName(className.replace('/', '.'), false, this.getClass().getClassLoader()/*RemappedMethods.loader*/);
+            Class<?> extend = reference.getSuperclass();
+            if (extend != null) {
+                parents.add(reverseMap(extend));
             }
 
-            for (String inter : visitor.getInterfaces()) {
+            for (Class<?> inter : reference.getInterfaces()) {
                 if (inter != null) {
-                    parents.add(reverseMap(mapper.map(inter)));
+                    parents.add(reverseMap(inter));
                 }
             }
 
             return parents;
-
         } catch (Exception e) {
-            System.err.println("Failed to read class for parents: " + className);
+            // Empty catch block
         }
-
         return null;
     }
-
-    private static class ParentVisitor extends ClassVisitor {
-        private String superName;
-        private String[] interfaces;
-
-        public ParentVisitor() {
-            super(Opcodes.ASM9);
-        }
-
-        @Override
-        public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-            this.superName = superName;
-            this.interfaces = interfaces;
-            super.visit(version, access, name, signature, superName, interfaces);
-        }
-
-        public String getSuperName() {
-            return superName;
-        }
-
-        public String[] getInterfaces() {
-            return interfaces == null ? new String[0] : interfaces;
-        }
-    }
-
 }
