20b95bfea1f6056b663d5b6c6f9da505ab7f1c5b
JustCursed <mytojimbyylabu@gmail.com>
2026-08-07 22:11:49 +0300

    remove timings

--- src/main/java/org/bukkit/craftbukkit/v1_12_R1/CraftServer.java
+++ src/main/java/org/bukkit/craftbukkit/v1_12_R1/CraftServer.java
@@ -288,7 +288,6 @@ public final class CraftServer implements Server {
         saveCommandsConfig();
         overrideAllCommandBlockCommands = commandsConfiguration.getStringList("command-block-overrides").contains("*");
         unrestrictedAdvancements = commandsConfiguration.getBoolean("unrestricted-advancements");
-        pluginManager.useTimings(configuration.getBoolean("settings.plugin-profiling"));
         monsterSpawn = configuration.getInt("spawn-limits.monsters");
         animalSpawn = configuration.getInt("spawn-limits.animals");
         waterAnimalSpawn = configuration.getInt("spawn-limits.water-animals");
